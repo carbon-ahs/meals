e9ed64ab7764dac0a198fd43801df76aea53360b
carbon-ahs <shehanuk.ahsan@gmail.com>
2023-10-31 12:26:53 +0600

    added mealScreen with dummyMeals and image 10/31/2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/meals.dart';
 
@@ -24,11 +24,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      // home: CatagoriesScreen(),
-      home: MealsScreen(
-        title: 'gerg',
-        meals: [],
-      ),
+      home: CatagoriesScreen(),
+      // home: const MealsScreen(
+      //   title: 'gerg',
+      //   meals: dummyMeals,
+      // ),
     );
   }
 }
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CatagoriesScreen extends StatelessWidget {
   const CatagoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    //Navigator.push(context, route);
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) =>
+            MealsScreen(title: category.title, meals: dummyMeals),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +35,12 @@ class CatagoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map((category) => CatagoryGridItem(category: category)).toList();
           for (final category in availableCategories)
-            CatagoryGridItem(category: category)
+            CatagoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
